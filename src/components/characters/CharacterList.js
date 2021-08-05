cf9f1d318157c00b2dd6d15f574f41739e6ccc68
Tis Lais <tistististis@gmail.com>
2021-08-05 12:36:37 -0700

    Add character list style to ul and refactor

--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -3,20 +3,44 @@ import { Link } from 'react-router-dom';
 import { useCharacters } from '../../state/characters';
 import Character from './Character';
 
+const li = `
+  p-5 
+  bg-white 
+  rounded 
+  shadow-md 
+  transform
+  hover:scale-105 
+  transition 
+  duration-300
+  ease-in-out 
+  cursor-pointer
+`;
+
+const ul = `
+  grid 
+  sm:grid-cols-2 
+  md:grid-cols-3 
+  lg:grid-cols-4 
+  xl:grid-cols-5 
+  2xl:grid-cols-6 
+  p-4 
+  gap-4
+`;
+
 const CharacterList = () => {
   const { characters, loading } = useCharacters();
   if (loading) return <h1>Loading...</h1>
 
   const characterElements = characters.map((character, i) => (
     <li key={character._id} 
-      className="p-5 bg-white rounded shadow-md hover:scale-105 transition duration-300 ease-in-out cursor-pointer">
+      className={li}>
       <Link to={`/character/${character._id}`}>
         <Character {...character} />
       </Link>
     </li>
   ));
   
-  return <ul className="gap-4">{characterElements}</ul>;
+  return <ul className={ul}>{characterElements}</ul>;
 }
 
 export default CharacterList;
