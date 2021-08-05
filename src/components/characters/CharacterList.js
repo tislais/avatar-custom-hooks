706876ef46cc647dac4e24de5abe34eea52d0d52
Tis Lais <tistististis@gmail.com>
2021-08-05 10:53:37 -0700

    Use correct arguments in CharacterList

--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom';
 import { useCharacters } from '../../state/characters';
 import Character from './Character';
 
+
+
 const CharacterList = () => {
   const characters = useCharacters();
 
   const characterElements = characters.map((character, i) => (
-    <li key={character.id} 
+    <li key={character._id} 
       className="p-5 bg-white rounded shadow-md hover:scale-105 transition duration-300 ease-in-out cursor-pointer">
-      <Link to={`/character/${character.id}`}>
-        <Character {...Character} />
+      <Link to={`/character/${character._id}`}>
+        <Character {...character} />
       </Link>
     </li>
   ));
   
-  return <ul>{characterElements}</ul>;
+  return <ul className="gap-4">{characterElements}</ul>;
 }
 
 export default CharacterList;
