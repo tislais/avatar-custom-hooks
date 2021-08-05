fd5fc1667bbcc0a1b3741dacfd0aaa1491b0a41d
Tis Lais <tistististis@gmail.com>
2021-08-05 10:51:40 -0700

    Add BrowserRouter to App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import CharacterList from '../characters/CharacterList';
 import CharacterDetails from '../details/CharacterDetails';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={CharacterList} />
-
-      <Route path="/character/:id" component={CharacterDetails} />
-
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={CharacterList} />
+        <Route path="/character/:id" component={CharacterDetails} />
+      </Switch>
+    </Router>
   );
 }
 
